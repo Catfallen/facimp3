b5ef618c9909e5d1d159bbcd829083828e70c13a
Catfallen <markimprogramador2006@gmail.com>
2025-03-18 20:38:58 -0300

    banco de dados atividade 2

--- android/aula4/my-app/app/index.tsx
+++ android/aula4/my-app/app/index.tsx
@@ -1,18 +1,28 @@
-import { Text, View } from "react-native";
-import { WebView } from "react-native-webview";
+import { useState } from "react";
+
+import { Text, View, Button } from "react-native";
+
+
 
 export default function Index() {
+  const dices = ["⚀","⚁","⚂","⚃","⚄","⚅"]
+  const [texto,setTexto] = useState(dices[1])
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
         flexDirection: "column"
       }}
     >
-      <Text>Markim</Text>
-      <Text>Markim</Text>
+      <Text style={{ fontSize: 48, color: "black", fontWeight: "bold", marginBottom: 40 }}>
+        Lançamento de dados
+      </Text>
+      <Text style={{ fontSize: 96, color: "black", fontWeight: "bold", marginBottom: 20 }}>{texto}</Text>
+      <Button title="Lançar dado" onPress={() => {
+        setTexto(dices[2])
+      }} />
     </View>
   );
 }
